dc3c10ea37e15e90087493c43a9e25a41b63d943
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-02-14 13:04:47 +0100

    set up error handlers

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-
+// import packages
 const express = require('express');
 const cors = require('cors');
 
-const { initDB } = require('./dbInit');
+const db = require('./dbInit');
 
 const app = express();
-initDB();  // check if db is connected
+db.initDB();  // check if db is connected
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ origin: process.env.DOMAIN, credentials: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+// catch 404 and forward to error handler
+app.all('*', (req, res, next) => {
+    let err = new Error('Page Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).json(err);
 });
 
 const PORT = process.env.PORT || 5000;
