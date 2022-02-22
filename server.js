9e7e4c5ddd09f1f9551e239cd4b17635f325d076
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-02-22 13:27:02 +0100

    adjust server.js to remove import view engine

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const LocalStrategy = require('passport-local').Strategy;
 const crypto = require('crypto');
 const session = require('express-session');
 const path = require('path');
-const ejsMate = require('ejs-mate');
 
 const db = require('./dbInit');
 const User = db.User;
@@ -22,9 +21,6 @@ const serviceRouters = require('./routers/service.routers');
 const app = express();
 app.use(cors({ origin: process.env.DOMAIN, credentials: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 db.initDB();  // check if db is connected
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
